edfb59fa5158a002281d8e4b2c4fd8fdf2060b06
valentinab25 <valentinab25>
2024-10-14 20:58:26 +0300

    Add prometheus, grafana configs

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,14 @@
-FROM debian:10-slim
+FROM debian:11-slim
 
 RUN apt-get update -q \
- && apt-get install -y --no-install-recommends ca-certificates unzip git wget \
+ && apt-get install -y --no-install-recommends curl jq ca-certificates unzip git wget \
  && apt-get clean \
  && rm -rf /var/lib/apt/lists/* 
 
 
 COPY docker-entrypoint.sh /
+COPY prometheus.yml /
+COPY grafanadashboard.json /
 
 ENTRYPOINT ["/docker-entrypoint.sh"]
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -56,7 +56,12 @@ fi
 # make sure prometheus has correct permissions
 if [ -d /etc/prometheus ]; then
 	chown 65534:65534 /etc/prometheus
-	chmod 755 /etc/prometheus
+  if [ ! -f /etc/prometheus/prometheus.yml ]; then
+	  cp /prometheus.yml /etc/prometheus/prometheus.yml
+	  chown 65534:65534 /etc/prometheus/prometheus.yml
+
+  fi
+
 fi
 
 # make sure grafana has correct permission
@@ -64,4 +69,19 @@ if [ -d /var/lib/grafana ]; then
 
 	chown 472:0 /var/lib/grafana
 	chmod 755 /var/lib/grafana
+
+
+     check_datasources=$(curl -s "http://grafana:3000/api/datasources"  -u admin:${GF_SECURITY_ADMIN_PASSWORD})
+    
+     if [ $(echo $check_datasources  | grep prometheus | wc -l) -eq 0 ]; then
+
+     curl -X "POST" "http://grafana:3000/api/datasources" \
+     -H "Content-Type: application/json" \
+     --user admin:${GF_SECURITY_ADMIN_PASSWORD} \
+     --data '{"id":1,"uid":"de0jejoon5beod","orgId":1,"name":"prometheus","type":"prometheus","typeName":"Prometheus","typeLogoUrl":"public/app/plugins/datasource/prometheus/img/prometheus_logo.svg","access":"proxy","url":"http://prometheus:9090","user":"","database":"","basicAuth":false,"isDefault":true,"jsonData":{"httpMethod":"POST"},"readOnly":false}'
+
+      curl -X POST --user admin:${GF_SECURITY_ADMIN_PASSWORD} -H "Content-Type: application/json" -d @/grafanadashboard.json http://grafana:3000/api/dashboards/import
+
+
+     fi
 fi
